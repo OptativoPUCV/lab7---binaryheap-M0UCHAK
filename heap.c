fe04fed490848f8f6de0fe1f738b662b396e8f32
Replit user <>
2023-09-28 21:14:46 +0000

    new try

--- heap.c
+++ heap.c
@@ -25,71 +25,71 @@ void* heap_top(Heap* pq){
 
 void heap_push(Heap* pq, void* data, int p) {
     
-    if (pq == NULL) return;
+  if (pq == NULL) return;
 
-    if (pq->size >= pq->capac) {
-        pq->capac = pq->capac * 2 + 1;
-        pq->heapArray = (heapElem*)realloc(pq->heapArray, sizeof(heapElem) * pq->capac);
+  if (pq->size >= pq->capac) {
+    pq->capac = pq->capac * 2 + 1;
+    pq->heapArray = (heapElem*)realloc(pq->heapArray, sizeof(heapElem) * pq->capac);
 
-        if (pq->heapArray == NULL) exit(EXIT_FAILURE);
-    }
+    if (pq->heapArray == NULL) exit(EXIT_FAILURE);
+  }
 
-    int currentIndex = pq->size;
-    pq->heapArray[currentIndex].data = data;
-    pq->heapArray[currentIndex].priority = p;
-    pq->size++;
+  int currentIndex = pq->size;
+  pq->heapArray[currentIndex].data = data;
+  pq->heapArray[currentIndex].priority = p;
+  pq->size++;
 
-    while (currentIndex > 0) {
-        int parentIndex = (currentIndex - 1) / 2;
-        if (pq->heapArray[currentIndex].priority > pq->heapArray[parentIndex].priority) {
-            
-            heapElem temp = pq->heapArray[currentIndex];
-            pq->heapArray[currentIndex] = pq->heapArray[parentIndex];
-            pq->heapArray[parentIndex] = temp;
-            currentIndex = parentIndex;
-        } 
-        else break;
-    }
+  while (currentIndex > 0) {
+    int parentIndex = (currentIndex - 1) / 2;
+    
+    if (pq->heapArray[currentIndex].priority > pq->heapArray[parentIndex].priority) {
+      heapElem temp = pq->heapArray[currentIndex];
+      pq->heapArray[currentIndex] = pq->heapArray[parentIndex];
+      pq->heapArray[parentIndex] = temp;
+      currentIndex = parentIndex;
+    } 
+    else break;
+  }
 }
 
 void heap_pop(Heap* pq){
-    if (pq == NULL || pq->size == 0) return;
     
-    pq->size--;
-    pq->heapArray[0] = pq->heapArray[pq->size];
+  if (pq == NULL || pq->size == 0) return;  
+  pq->size--;
+  pq->heapArray[0] = pq->heapArray[pq->size];
+  
+  int currentIndex = 0;
+  
+  while (1) {
+    int leftChildIndex = 2 * currentIndex + 1;
+    int rightChildIndex = 2 * currentIndex + 2;
+    int largestIndex = currentIndex;
 
-    int currentIndex = 0;
-    while (1) {
-        int leftChildIndex = 2 * currentIndex + 1;
-        int rightChildIndex = 2 * currentIndex + 2;
-        int largestIndex = currentIndex;
-
-      if (leftChildIndex < pq->size &&
-            pq->heapArray[leftChildIndex].priority > pq->heapArray[largestIndex].priority) {
-            largestIndex = leftChildIndex;
-      }
-      if (rightChildIndex < pq->size &&
-            pq->heapArray[rightChildIndex].priority > pq->heapArray[largestIndex].priority) {
-            largestIndex = rightChildIndex;
-      }
-      if (largestIndex != currentIndex) {
-            heapElem temp = pq->heapArray[currentIndex];
-            pq->heapArray[currentIndex] = pq->heapArray[largestIndex];
-            pq->heapArray[largestIndex] = temp;
-            currentIndex = largestIndex;
-        } else break;
+    if (leftChildIndex < pq->size && 
+      pq->heapArray[leftChildIndex].priority > pq->heapArray[largestIndex].priority) {
+      largestIndex = leftChildIndex;
     }
+    if (rightChildIndex < pq->size && pq->heapArray[rightChildIndex].priority > pq->heapArray[largestIndex].priority) {
+      largestIndex = rightChildIndex;
+    }
+    if (largestIndex != currentIndex) {
+      heapElem temp = pq->heapArray[currentIndex];
+      pq->heapArray[currentIndex] = pq->heapArray[largestIndex];
+      pq->heapArray[largestIndex] = temp;
+      currentIndex = largestIndex;
+    } else break;
+  }
 }
 
 Heap* createHeap(){
 
-Heap* newHeap = (Heap*)malloc(sizeof(Heap));
+  Heap* newHeap = (Heap*)malloc(sizeof(Heap));
 
-if (newHeap == NULL) exit(EXIT_FAILURE);
+  if (newHeap == NULL) exit(EXIT_FAILURE);
     
-newHeap->capac = 3;
-newHeap->size = 0;
-newHeap->heapArray = (heapElem*)malloc(sizeof(heapElem) * newHeap->capac);
+    newHeap->capac = 3;
+    newHeap->size = 0;
+    newHeap->heapArray = (heapElem*)malloc(sizeof(heapElem) * newHeap->capac);
 
   if (newHeap->heapArray == NULL) {
     free(newHeap);
